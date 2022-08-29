c6fa5550ebf2dda65d209ae8386542e82438682e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-08-29 11:14:09 -1000

    feat: add Navigation Custom Type and NavigationItem Slice

---
+++ .slicemachine/assets/slices/NavigationItem/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/NavigationItem';
+
+export default {
+  title: 'slices/NavigationItem'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"name":[{"type":"heading6","text":"Divide","spans":[]}],"link":{"link_type":"Web","url":"https://prismic.io"}}],"primary":{"name":[{"type":"heading3","text":"Forest","spans":[]}],"link":{"link_type":"Web","url":"https://slicemachine.dev"}},"slice_type":"navigation_item","id":"_Default"}} />
+_Default.storyName = ''
