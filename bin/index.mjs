169549ac39f6913107ffd9bfad770dc489932f3e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-09-07 16:35:10 -1000

    chore: fix script

--- bin/index.mjs
+++ bin/index.mjs
@@ -2,6 +2,7 @@
 
 import * as fs from "fs";
 import * as path from "path";
+import * as url from "url";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const cwd = process.cwd();
